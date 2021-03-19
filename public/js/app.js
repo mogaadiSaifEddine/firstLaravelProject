53eed4a009c6856cacb4dbd2d7fdafa6d4aa9803
saifmogaadi12 <aboura.joujou@outlook.es>
2021-03-19 16:48:56 +0100

    changing home to dashboard

--- public/js/app.js
+++ public/js/app.js
@@ -7,60 +7,7 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./resources/js/app.js":
-/*!*****************************!*\
-  !*** ./resources/js/app.js ***!
-  \*****************************/
-/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
-
-eval("__webpack_require__(/*! ./bootstrap */ \"./resources/js/bootstrap.js\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9yZXNvdXJjZXMvanMvYXBwLmpzPzZkNDAiXSwibmFtZXMiOlsicmVxdWlyZSJdLCJtYXBwaW5ncyI6IkFBQUFBLG1CQUFPLENBQUMsZ0RBQUQsQ0FBUCIsImZpbGUiOiIuL3Jlc291cmNlcy9qcy9hcHAuanMuanMiLCJzb3VyY2VzQ29udGVudCI6WyJyZXF1aXJlKCcuL2Jvb3RzdHJhcCcpO1xuIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./resources/js/app.js\n");
-
-/***/ }),
-
-/***/ "./resources/js/bootstrap.js":
-/*!***********************************!*\
-  !*** ./resources/js/bootstrap.js ***!
-  \***********************************/
-/***/ (() => {
-
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module 'C:\\Users\\saif\\lsapp\\node_modules\\@babel\\helper-compilation-targets\\lib\\index.js'\n    at createEsmNotFoundErr (node:internal/modules/cjs/loader:952:15)\n    at finalizeEsmResolution (node:internal/modules/cjs/loader:945:15)\n    at resolveExports (node:internal/modules/cjs/loader:473:14)\n    at Function.Module._findPath (node:internal/modules/cjs/loader:513:31)\n    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:911:27)\n    at Function.Module._load (node:internal/modules/cjs/loader:769:27)\n    at Module.require (node:internal/modules/cjs/loader:997:19)\n    at require (C:\\Users\\saif\\lsapp\\node_modules\\v8-compile-cache\\v8-compile-cache.js:159:20)\n    at _helperCompilationTargets (C:\\Users\\saif\\lsapp\\node_modules\\@babel\\core\\lib\\config\\validation\\option-assertions.js:28:16)\n    at assertTargets (C:\\Users\\saif\\lsapp\\node_modules\\@babel\\core\\lib\\config\\validation\\option-assertions.js:285:11)");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./resources/js/app.js");
-/******/ 	
+/******/ 	"use strict";
+var __webpack_exports__ = {};
 /******/ })()
 ;
--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,5 @@ Route::resource('posts', PostsController::class);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index']);
+
